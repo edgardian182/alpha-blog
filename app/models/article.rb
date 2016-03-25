56023632256a6abed1edbefd4ae36167febc3b05
Andres <edgarquintana984@hotmail.com>
2016-03-25 15:56:29 +0000

    Arreglado error de eliminar

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :user 
-  validates :title, presence: true, length: {minimum: 3, maximum: 50} 
+  validates :title, presence: {message: "debe estar presente"}, length: {minimum: 3, maximum: 50, message: "es muy corto, minimo 3 caracteres"} 
   validates :description, presence: true, length: {minimum: 10, maximum: 50}
   validates :user_id, presence: true
 end
