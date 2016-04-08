ceca0886950cef492ebf025e3a1a1d5d3758be1c
Andres <edgarquintana984@hotmail.com>
2016-04-08 04:27:20 +0000

    Add Categories to NavBar, require_admin and fix test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
   
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "Edgar", email: "edgar@gmail.com", password: "password", admin: true)
   end
   
   test "should get index" do
@@ -12,6 +13,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -22,10 +24,17 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    session[:user_id] = @user.id
     get :edit, id: @category
     assert_response :success
   end
 
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: {name: "sports"}
+    end
+    assert_redirected_to categories_path
+  end
 
 end
 
