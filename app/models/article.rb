35fb6c3f9ef63911b8fc9dc47da0cc30ca4f1f3c
Andres <edgarquintana984@hotmail.com>
2016-04-08 23:29:45 +0000

    article_category Model creado y su asociacion con Article y Category, vistas relacionadas

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
+  
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  
   belongs_to :user 
   validates :title, presence: {message: "debe estar presente"}, length: {minimum: 3, maximum: 50, message: "es muy corto, minimo 3 caracteres"} 
   validates :description, presence: true, length: {minimum: 10, maximum: 50}
