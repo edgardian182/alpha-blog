11958ef16f45153cfd99f3b59a21c521f990d92a
Andres <edgarquintana984@hotmail.com>
2016-04-09 01:00:00 +0000

    Edicion de categorias ready

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
   before_action :require_admin, except: [:index, :show]
   
   def index
@@ -21,13 +22,21 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
   end
 
+  def update
+    if @category.update(category_params)
+      flash[:success] = "The category has been successfully updated"
+      redirect_to categories_path
+    else
+      render 'edit'
+    end
+  end
+  
   def destroy
   end
   
@@ -42,6 +51,10 @@ class CategoriesController < ApplicationController
       flash[:danger] = "Only admins can perform that action"
       redirect_to categories_path
     end
+  end
+  
+  def set_category
+    @category = Category.find(params[:id])
   end
   
   
